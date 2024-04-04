585939c9d71b9cf0d9f0616cab8736c40b4667fe
OanhLv <laioanh961@gmail.com>
2024-04-05 00:01:46 +0700

    hello world

--- Lesson2/Bai1.cpp
+++ Lesson2/Bai1.cpp
@@ -1,3 +1,19 @@
 #include <iostream>
 
 using namespace std;
+
+int main(){
+    int IsHelloWorld;
+    cin >> IsHelloWorld;
+    if (IsHelloWorld != 0)
+    {
+        cout << "Hello world\n";
+        cout << "This is value =" << IsHelloWorld << endl;
+    }
+    else
+    {
+        cout << "Nothing....\n";
+        cout << "This is value =" << IsHelloWorld << endl;
+    }
+    return 0;
+}
